814dba9c2d78d3a2aa22175c99b28f24d60df8ed
MunSunch <msunchalyaev@gmail.com>
2023-04-02 19:29:38 +0400

    Работа с DOM деревом

--- Урок1/main2.js
+++ Урок1/main2.js
@@ -5,7 +5,32 @@ div.classList.add('box-num')
 body.append(div)
 
 // Найти и изменить элемент
-let ul = document.getElementsByClassName("text")
-let array = String(ul.).split(' ')
+let text = document.getElementsByClassName("text")
+let array = String(text).split(' ')
 console.log(array)
-console.log(ul)
+console.log(text)
+
+
+let btnPlus = document.createElement('button')
+btnPlus.classList.add("plus")
+btnPlus.textContent = '+'
+
+let btnMunis = document.createElement('button')
+btnMunis.classList.add("minus")
+btnMunis.textContent = '-'
+
+let span = document.createElement('span')
+span.textContent = 0
+
+div.append(btnPlus, span, btnMunis)
+
+
+
+// Обработчик событий
+btnPlus.addEventListener('click', function() {
+    span.textContent++
+})
+
+btnMunis.addEventListener('click', function() {
+    span.textContent--
+})
